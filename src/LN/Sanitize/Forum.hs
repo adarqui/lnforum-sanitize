76fe4e46ca5813bab08658e51834521cb9733824
Andrew Darqui <andrew.darqui@gmail.com>
2016-07-05 14:02:33 -0400

    forum sanitizer

---
+++ src/LN/Sanitize/Forum.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module LN.Sanitize.Forum (
+  sanitizeForumRequest
+) where
+
+
+
+import           LN.Sanitize.Internal (sanitizeLine)
+import           LN.T.Forum.Request   (ForumRequest (..))
+
+
+
+sanitizeForumRequest :: ForumRequest -> ForumRequest
+sanitizeForumRequest (ForumRequest display_name m_desc threads_per_board posts_per_thread recent_threads_limit recent_posts_limit motw_limit icon tags visibility guard) =
+  ForumRequest
+    (sanitizeLine display_name)
+    (sanitizeLine <$> m_desc)
+    threads_per_board
+    posts_per_thread
+    recent_threads_limit
+    recent_posts_limit
+    motw_limit
+    icon
+    (map sanitizeLine tags)
+    visibility
+    guard
